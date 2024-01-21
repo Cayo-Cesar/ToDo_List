00d400e8c67e6c5ea7f48afc99d0b1ec6798b0e9
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-01-21 19:33:39 -0300

    Color and Icon

--- toDo.py
+++ toDo.py
@@ -15,53 +15,43 @@ def delTask():
     except IndexError:
         pass
 
-def saveList():
-    with open("tasks.txt", "w") as f:
-        tasks = listbox.get(0, listbox.size())
-        for task in tasks:
-            f.write(task + "\n")
-
-def loadList():
-    try:
-        with open("tasks.txt", "r") as f:
-            for task in f:
-                listbox.insert(tk.END, task.strip())
-    except FileNotFoundError:
-        pass
-
 window = tk.Tk()
 window.title("To-Do List")
-window.geometry("500x500")
+window.iconbitmap('check.ico')
+window.geometry("500x400")
 window.configure(bg="gray")  # Set the background color of the window to gray
 
-frame_main = tk.Frame(window, bg="gray")
+frame_main = tk.Frame(window, bg="#FFEFDB")
 frame_main.pack(fill=tk.BOTH, expand=True)
 
-frame_buttons = tk.Frame(frame_main, bg="gray")
+frame_buttons = tk.Frame(frame_main, bg="#FFEFDB")
 frame_buttons.pack(side=tk.LEFT, padx=10)
 
-text_title = tk.Label(frame_buttons, text="To-Do List", font=("Script", 40), bg="gray", fg="black")
+text_title = tk.Label(frame_buttons, text="To-Do List", font=("Script", 40), bg="#FFEFDB", fg="black")
 text_title.pack(pady=10)
 
-entry = tk.Entry(frame_buttons, font=("Helvetica", 12), justify=tk.CENTER, bg="gray", fg="black")
+text_title = tk.Label(frame_buttons, text="Adicione uma tarefa", font=("Times", 12), bg="#FFEFDB", fg="black", justify=tk.CENTER)
+text_title.pack(pady=10)
+
+entry = tk.Entry(frame_buttons, font=("Times", 12), justify=tk.CENTER, bg="#FAEBD7", fg="black")
 entry.pack(pady=10)
 
-button_add_task = tk.Button(frame_buttons, text="Adicionar Tarefa", width=25, command=addTask, bg="gray", fg="black", activebackground="gray", activeforeground="black")
+button_add_task = tk.Button(frame_buttons, text="Adicionar Tarefa", font="Times" , width=25, command=addTask, bg="#FFEFDB", fg="black", activebackground="#FFEFDB", activeforeground="black")
 button_add_task.pack(pady=5)
 
-button_del_task = tk.Button(frame_buttons, text="Deletar Tarefa", width=25, command=delTask, bg="gray", fg="black", activebackground="gray", activeforeground="black")
+button_del_task = tk.Button(frame_buttons, text="Deletar Tarefa", font="Times" ,width=25, command=delTask, bg="#FFEFDB", fg="black", activebackground="gray", activeforeground="black")
 button_del_task.pack(pady=5)
 
-button_del_all_tasks = tk.Button(frame_buttons, text="Deletar Todas as Tarefas", width=25, command=lambda: listbox.delete(0, tk.END), bg="gray", fg="black", activebackground="gray", activeforeground="black")
+button_del_all_tasks = tk.Button(frame_buttons, text="Deletar Todas as Tarefas", font="Times" , width=25, command=lambda: listbox.delete(0, tk.END), bg="#FFEFDB", fg="black", activebackground="gray", activeforeground="black")
 button_del_all_tasks.pack(pady=5)
 
-button_exit = tk.Button(frame_buttons, text="Sair", width=25, command=window.destroy, bg="gray", fg="black", activebackground="gray", activeforeground="black")
+button_exit = tk.Button(frame_buttons, text="Sair", font="Times" , width=10, command=window.destroy, bg="#FFEFDB", fg="black", activebackground="gray", activeforeground="black")
 button_exit.pack(pady=5)
 
-frame_listbox = tk.Frame(frame_main, bg="gray")
+frame_listbox = tk.Frame(frame_main, bg="#FAEBD7")
 frame_listbox.pack(side=tk.RIGHT, padx=10, fill=tk.BOTH, expand=True)
 
-listbox = tk.Listbox(frame_listbox, height=15, width=25, font=("Helvetica", 12), justify=tk.CENTER, bg="gray", fg="black")
+listbox = tk.Listbox(frame_listbox, height=15, width=25, font=("Helvetica", 12), justify=tk.CENTER, bg="#FAEBD7", fg="black")
 listbox.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
 scrollbar_tasks = tk.Scrollbar(frame_listbox, bg="gray")
